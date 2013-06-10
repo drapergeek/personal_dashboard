6d1547d63111df5508609741bd0c74580c359a57
Jason Draper <jason@drapergeek.com>
2013-06-09 20:55:19 -0400

    Clean up the travisci dashboards.

--- jobs/travisci.rb
+++ jobs/travisci.rb
@@ -1,8 +1,7 @@
 require 'travis'
 require 'travis/pro'
 
-def update_builds(repository, config)
-  builds = []
+def build_info(repository, config)
   repo = nil
 
   if config["type"] == "pro"
@@ -14,27 +13,24 @@ def update_builds(repository, config)
   end
 
   build = repo.last_build
-  build_info = {
-    label: "Build #{build.number}",
-    value: "[#{build.branch_info}], #{build.state} in #{build.duration}s",
-    state: build.state
+  {
+    branch: "Build #{build.number}",
+    value: "#{build.branch_info}",
+    duration_state: "#{build.state} in #{build.duration}s"
   }
-  builds << build_info
-
-  builds
 end
 
 config_file = File.dirname(File.expand_path(__FILE__)) + '/../config/travisci.yml'
 config = YAML::load(File.open(config_file))
 
-SCHEDULER.every('2m', first_in: '1s') {
+SCHEDULER.every('2m', first_in: '1s') do
   config.each do |type, type_config|
     unless type_config["repositories"].nil?
       type_config["repositories"].each do |data_id, repo|
-        send_event(data_id, { items: update_builds(repo, type_config) })
+        send_event(data_id, build_info(repo, type_config))
       end
     else
       puts "No repositories for travis.#{type}"
     end
   end
-}
+end
