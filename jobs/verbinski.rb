8cc6d336c2c1a5f1b8f76a997260bb33b18eda1b
Jason Draper <jason@drapergeek.com>
2013-12-27 13:38:44 -0500

    Minor cleanup to the forecast.

--- jobs/verbinski.rb
+++ jobs/verbinski.rb
@@ -14,25 +14,25 @@ forecast_units = "us" # like "si", except windSpeed is in kph
 
 def time_to_str(time_obj)
   """ format: 5 pm """
-  return Time.at(time_obj).strftime "%-l %P"
+ Time.at(time_obj).strftime "%-l %P"
 end
 
 def time_to_str_minutes(time_obj)
   """ format: 5:38 pm """
-  return Time.at(time_obj).strftime "%-l:%M %P"
+  Time.at(time_obj).strftime "%-l:%M %P"
 end
-  
+
 def day_to_str(time_obj)
   """ format: Sun """
-  return Time.at(time_obj).strftime "%a"
+  Time.at(time_obj).strftime "%a"
 end
-  
+
 SCHEDULER.every '30m', :first_in => 0 do |job|
   http = Net::HTTP.new("api.forecast.io", 443)
   http.use_ssl = true
   http.verify_mode = OpenSSL::SSL::VERIFY_PEER
   response = http.request(Net::HTTP::Get.new("/forecast/#{forecast_api_key}/#{forecast_location_lat},#{forecast_location_long}?units=#{forecast_units}"))
-  forecast = JSON.parse(response.body)  
+  forecast = JSON.parse(response.body)
 
   currently = forecast["currently"]
   current = {
@@ -66,7 +66,7 @@ SCHEDULER.every '30m', :first_in => 0 do |job|
     this_week.push(this_day)
   end
 
-  send_event('verbinski', { 
+  send_event('verbinski', {
     current: current,
     today: today,
     upcoming_week: this_week,
